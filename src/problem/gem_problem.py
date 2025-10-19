f6a0088e89604cc033d00aa075595244f62a4c20
mari-39 <Marievictoria@gmx.de>
2025-10-19 20:55:18 +0200

    fixed gemProblem heuristic

--- src/problem/gem_problem.py
+++ src/problem/gem_problem.py
@@ -6,19 +6,26 @@ class GemProblem(SearchProblem[WorldState]):
     # first collect all gems
     # then reach exit
     def is_goal_state(self, state: WorldState) -> bool:
+        # if all gems collected + all agents in exit posn
         raise NotImplementedError()
 
     def heuristic(self, problem_state: WorldState) -> float:
         agentPosnList = problem_state.agents_positions
         exitPosnList = self.world.exit_pos
-        gemPosnList = [gem.pos for gem in self.world.gems]
-        
-        def distance(agentPosn: tuple[int, int], goal: tuple[int, int]) -> int:
-            d_x = abs(agentPosn[0] - goal[0])
-            d_y = abs(agentPosn[1] - goal[1])
-            return d_x + d_y
+        gemPosnList = [gem.pos 
+            for gem, collected_status in zip(self.world.gems, problem_state.gems_collected) 
+            if not collected_status]
         
         if (not(all(problem_state.gems_collected))):
-            return sum(min(distance(agentPosn, gemPosn) for gemPosn in gemPosnList) for agentPosn in agentPosnList)
+            min_dist_collection = min(self.distance(agentPosn, gemPosn) # COST OF ONE GEM COLLECTION / START: we need to collect min. 1 gem, make it the cheapest
+                                    for agentPosn in agentPosnList
+                                    for gemPosn in gemPosnList)
+            
+            min_dist_gem_exit = min(self.distance(gemPosn, exitPosn) # COST OF EXIT: we end up at a gem and look at the best-case distance of the last gem to the nearest exit 
+                for exitPosn in exitPosnList 
+                for gemPosn in gemPosnList)
+        
+            return  min_dist_collection + min_dist_gem_exit
+        
         else:
-            return sum(min(distance(agentPosn, exitPosn) for exitPosn in exitPosnList) for agentPosn in agentPosnList)
+            return sum(min(self.distance(agentPosn, exitPosn) for exitPosn in exitPosnList) for agentPosn in agentPosnList)
